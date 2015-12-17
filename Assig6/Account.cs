6ba9223cd3d2dd9a8f773ab3ddbade4d86bc04ed
Daniel.S <daniel.sindrestean@gmail.com>
2015-12-17 19:55:10 +0100

    Something

---
+++ Assig6/Account.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assig6
+{
+    class Account
+    {
+        private static double balance;
+        private double dailyAmount;
+
+        public Account(double amount)
+        {
+            Balance = amount;
+        }
+
+        public Account()
+        {
+            Balance = 100;
+        }
+
+        [ContractInvariantMethod]
+        private void DailyBalanceChecker()
+        {
+            Contract.Invariant(dailyAmount < 5000, "Your use would be above your daily limit");
+            Contract.Invariant(Balance > 50, "Your method sucks");
+        }
+
+        public void Deposit(double amount)
+        {
+            Contract.Requires(amount > 0);
+            Contract.Ensures(Contract.OldValue(Balance) + amount == Balance);
+            Balance += amount;
+        }
+
+        public void Withdraw(double amount)
+        {
+            Contract.Requires(amount > 0);
+            Contract.Ensures(Contract.OldValue(Balance) - amount == Balance);
+            if (amount > Balance) throw new InvalidOperationException("The amount is bigger then your balance");
+            Balance -= amount;
+        }
+
+
+        public static double Balance
+        {
+            get
+            {
+                return balance;
+            }
+
+            set
+            {
+                balance = value;
+            }
+        }
+    }
+}
---
+++ Assig6/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assig6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account = new Account(250);
+            account.Deposit(100);
+            account.Withdraw(300);
+            account.Withdraw(100);
+            account.Deposit(-100);
+            account.Withdraw(-20);
+
+            Console.ReadLine();
+
+        }
+    }
+}
